9444643fb93d410bf6da3ccd2d919674c0550d12
pishexod <dimabukov52@gmail.com>
2023-12-08 13:38:19 +0200

    pm2

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary w-100">
     <div class="container-fluid">
-      <router-link to="/" class="navbar-brand">Dental</router-link>
+      <router-link to="/" class="navbar-brand">MOUK</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
               aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
